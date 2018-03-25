59482acce51607325d829d851e2089f05071b635
anhldpro <anhldpro@yahoo.com>
2018-03-25 15:23:42 +0700

    Init

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,19 @@
+import { SettingsPage } from './../settings/settings';
+import { Component } from '@angular/core';
+
+import { AboutPage } from '../about/about';
+import { HomePage } from '../home/home';
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tab1Root = HomePage;
+  tab2Root = AboutPage;
+  tab3Root = SettingsPage;
+
+  constructor() {
+
+  }
+}
